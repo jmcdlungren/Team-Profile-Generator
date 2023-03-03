5e33a1296c584017e7eb326c1c45b24e02b34e13
jmcdlungren <j.mcd.lungren@gmail.com>
2023-03-02 17:02:37 -0700

    updated classes, created prompts, started on HTML

--- index.js
+++ index.js
@@ -2,6 +2,139 @@ const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
+const renderTeam = require('./src/TeamBuilder.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const teamMembers = [];
 
 
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the manager's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the manager's id?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the manager's email?",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the manager's office number?",
+            }
+        ])
+        .then(response => {
+            const manager = new Manager(response.name, response.id, response.email, response.officeNumber)
+            teamMembers.push(manager)
+        })
+        .then(() => init())
+}
 
+function createEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's id?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email?",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the engineer's GitHub username?",
+            }
+        ])
+        .then(response => {
+            const engineer = new Engineer(response.name, response.id, response.email, response.github)
+            teamMembers.push(engineer)
+        })
+        .then(() => init())
+}
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's id?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email?",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the name of the intern's school?",
+            }
+        ])
+        .then(response => {
+            const intern = new Intern(response.name, response.id, response.email, response.school)
+            teamMembers.push(intern)
+        })
+        .then(() => init())
+}
+
+function buildTeam() {
+    // renderTeam(teamMembers);
+    fs.writeFileSync("./dist/index.html", renderTeam(teamMembers))
+}
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Which employee needs to be added?',
+                choices: ['Manager', 'Engineer', 'Intern', 'Finish building my team'],
+            }
+
+        ])
+        .then(response => {
+            console.log(response)
+
+            switch (response.employee) {
+                case 'Manager':
+                    createManager();
+                    break
+                case 'Engineer':
+                    createEngineer();
+                    break
+                case 'Intern':
+                    createIntern();
+                    break
+                default:
+                    buildTeam();
+            }
+        })
+}
+
+
+init();
---
+++ src/TeamBuilder.js
@@ -0,0 +1,50 @@
+function teamHTML(data) {
+    for(i = 0; i < data.length; i++) {
+        if(data[i].getRole() === 'Manager') {
+            return `<div class="card">
+        <h2>${data[i].getName()}</h2>
+        <p>${data[i].getRole()}</p>
+        </div>`
+        }
+        if(data[i].getRole() === 'Engineer') {
+            return `<div class="card">
+        <h2>${data[i].getName()}</h2>
+        <p>${data[i].getRole()}</p>
+        </div>`
+        }
+        if(data[i].getRole() === 'Intern') {
+            return `<div class="card">
+        <h2>${data[i].getName()}</h2>
+        <p>${data[i].getRole()}</p>
+        </div>`
+        }
+    }
+}
+
+
+function renderTeam(team) {
+    console.log(team)
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+      <title>Document</title>
+    </head>
+    <body>
+      <header class="p-5 mb-4 header bg-light">
+        <div class="container">
+          <h1>My Team</h1>
+        </div>
+      </header>
+
+      <main>
+        ${teamHTML(team)}
+      </main>
+    </body>
+    </html>`
+}
+
+
+module.exports = renderTeam;
